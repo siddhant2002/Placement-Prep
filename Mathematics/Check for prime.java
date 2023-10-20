80882edd6030a624f2ae7d62b77c74d3de6f81bd
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-10-20 14:00:31 +0530

    Files added

---
+++ Mathematics/Check for prime.java
@@ -0,0 +1,18 @@
+public class Solution {
+	public static boolean isPrime(int num) {
+		if(num==1)
+		{
+			return false;
+		}
+		boolean flag=true;
+		for(int i=2;i<=Math.sqrt(num);i++)
+		{
+			if(num%i==0)
+			{
+				flag=false;
+				break;
+			}
+		}
+		return flag ? "YES" : "NO";
+	}
+}
