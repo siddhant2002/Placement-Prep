0b9794bc934ca22bb8b88c2237d5794de633ab69
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-10-20 13:48:00 +0530

    Files added

---
+++ Mathematics/Count Digits.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public static int countDigits(int n){
+        int count=0,dup=n;
+        while(n!=0)
+        {
+            int d=(n%10);
+            n/=10;
+            if(d==0)
+            {
+                continue;
+            }
+            if(dup%d==0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
