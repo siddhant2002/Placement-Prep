e5b3910c966c4a83d296aa4562860f2fe7599905
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-10-28 23:29:22 +0530

    Files added

---
+++ LinkedList/Insert Node At The Beginning.java
@@ -0,0 +1,44 @@
+package LinkedList;
+
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node insertAtFirst(Node list, int newValue) {
+        Node fresh=new Node(newValue);
+        fresh.next=list;
+        list=fresh;
+        return list;
+    }
+}
