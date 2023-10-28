7a6db096e9dadfe13effd004d79836761a6d41c8
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-10-28 23:34:45 +0530

    Files added

---
+++ LinkedList/Delete Node Of Linked List.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution {
+    public static Node deleteLast(Node list){
+        Node ptr=list;
+        for(ptr=list;ptr.next.next!=null;ptr=ptr.next);
+        ptr.next=null;
+        return list;
+    }
+}
