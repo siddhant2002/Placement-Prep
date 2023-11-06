9ade08e08615ecf691327d28f8df438acd290f91
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-11-06 19:52:29 +0530

    Files added

---
+++ Array/Max Consecutive Ones.java
@@ -0,0 +1,20 @@
+package Array;
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxi=0,count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            {
+                maxi=Math.max(maxi,count);
+                count=0;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        return Math.max(maxi,count);
+    }
+}
