662a6797120188ee60afe845628a6df1bd4e1d18
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-11-06 19:50:36 +0530

    Files added

---
+++ Array/Missing Number.java
@@ -0,0 +1,12 @@
+package Array;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        for(int i:nums)
+        {
+            sum+=i;
+        }
+        return (nums.length*(nums.length+1))/2 - sum;
+    }
+}
